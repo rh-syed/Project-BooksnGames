3dba60dfb0448288d302e28db76029663e022354
eransyd <syed.rahman@ericsson.com>
2016-01-05 11:59:12 -0500

    register.php is for HTML and CSS for User Registration web page.

---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+include ("includes/connect.php");
+
+?>
+
+<!DOCTYPE html>
+
+<html lang ="en">
+
+<head>
+	<title> Register </title>
+	<link rel = "stylesheet" href = "css/main.css">
+</head>
+
+<body>
+	<div id = "wrapper">
+		<aside id = "left_side">
+		
+		</aside>
+		
+		<section id = "right_side">
+		
+		</section>
+	</div>
+</body>
+
+</html>
