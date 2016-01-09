8acf6f05fe0c5d3106871b0ab7f428a35c6fd7f5
eransyd <syed.rahman@ericsson.com>
2016-01-09 12:49:21 -0500

    CSS completed. html_codes.php completed. register.php page begun coding. images/registerbanner.png added.

--- html_codes.php
+++ html_codes.php
@@ -4,12 +4,13 @@ function headerAndSeachCode(){
 	$defaultText = htmlentities($_GET['keywords']);
 	
 	echo '
-		<header id ="main+header">
+		<header id ="main_header">
 			<div id="rightAlign">
 			
 	';
 	
-	//links will be here
+	//links will be here. Check if user is logged in or logged out. Depending on that link will be displayed.
+	topRightLinks(); 
 	echo "
 			</div>
 			<a href =\"index.php\"><img src = \"images/mainLogo.png\"></a>
@@ -21,13 +22,101 @@ function headerAndSeachCode(){
 				value=\"$defaultText\"> &nbsp;
 				
 				<select id=\"category\" name=\"category\" class=\"searchBox\">
-	";
+	"; //category id css is missing
+	
 	//include categories here
+	createCategoryList();
 	echo '
 				</select>
 				<input type="submit" value="Search" class="button" />
 			</form>
 		</div>
 	';
+}
+
+//Top Right Links
+function topRightLinks(){
+	//If user is logged out
+	if(!isset($_SESSION['user_id'])){
+		echo '<a href="register.php"> Register </a> | <a href="login.php"> Log In </a> '
+	}
+	else{//If user is logged in
+		$user = $_SESSION['user_id'];
+		$result = mysql_query("SELECT * FROM messages WHERE receiver = $user AND status = 'unread'" or die(mysql_error) );
+		
+		$num = mysql_num_rows($result );
+		if($num == 0){
+			echo '<a href = "messages_inbox.php">Messages</a> |';
+		}
+		else{
+			echo "<span class =\"usernames\"><a href = \"messages_inbox.php\">Messages($num)</a></span> |";
+		}
+		
+		echo '<a href="additem.php">Add Item</a> | <a href="account.php">My Account</a> | <a href="logout.php">Log out</a>';
+	}
+}
+
+//Create Category <option>'s for search bar
+
+function createCategoryList(){
+	
+	if ( ctype_digit($_GET['category'])) {
+		$x = $_GET['category'];
+	}
+	else{
+		$x = 999;
+	}
+	
+	echo "<option>All Categories</option>";
+	
+	$i= 0;
+	
+	while(1){
+		if(numberToCategory($i) == "Category Does Not Exist"){
+			break;
+		}
+		else{
+			echo " <option value = \"$i\" ";
+			if($i==$x)(echo 'SELECTED';)
+			echo " > ";
+			echo numberToCategory($i);
+			echo "</option>";
+		}
+		$i++;
+	}
+}
+//Category Number to String
+function numberToCategory($n){
+	switch ($n){
+		case 0:
+			$cat = "Autobiography";
+			break;
+		case 1:
+			$cat = "Biography";
+			break;
+		case 2: 
+			$cat = "Fantasy";
+			break;
+		case 3: 
+			$cat = "Historical Fiction";
+			break;
+		case 4:
+			$cat = "Informational";
+			break;
+		case 5:
+			$cat = "Mystery & Adventure";
+			break;
+		case 6: 
+			$cat = "Poetry";
+			break;
+		case 7: 
+			$cat = "Realistic Fiction";
+			break;
+		case 8: 
+			$cat = "Science Fiction";
+			break;
+		default:
+			$cat = "Category Does Not Exist";
+	}
 }
 ?>
--- register.php
+++ register.php
@@ -18,12 +18,33 @@ include ("includes/html_codes.php");
 
 <body>
 	<div id = "wrapper">
+		<?php headerAndSeachCode (); ?>
 		<aside id = "left_side">
-		
+			<img src = "images/registerbanner.png" />
 		</aside>
 		
 		<section id = "right_side">
-		
+			<form id = "generalform" class = "container" method ="post" action ="">
+				<h3> Register </h3>
+				<?php echo $error_message; ?>
+				<div class ="field">
+					<label for = ="username">Username:</label>
+					<input type="text" class="input" id="username" name="username" maxlength="20"/>
+					<p class ="hint">20 characters maximum (letters and numbers only)</p>
+				</div>	
+				
+				<div class ="field">
+					<label for = ="username">Email:</label>
+					<input type="text" class="input" id="email" name="email" maxlength="80"/>
+				</div>	
+				
+				<div class = "field">
+					<label for = ="username">Password:</label>
+					<input type="password" class="input" id="password" name="password" maxlength="20"/>
+					<p class ="hint">20 characters maximum </p>
+				</div>
+				<input type ="button" name="submit" id="submit" class="button" value="Register"/>
+			</form>
 		</section>
 	</div>
 </body>
