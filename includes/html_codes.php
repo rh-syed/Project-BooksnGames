956d73b81570f87c45263e08155feffe700421e3
eransyd <syed.rahman@ericsson.com>
2016-01-26 15:15:15 -0500

    fixed bugs in this code

--- includes/html_codes.php
+++ includes/html_codes.php
@@ -1,30 +1,22 @@
 <?php
 //Code for Header and Search Bar
-function headerAndSeachCode(){
+function headerAndSearchCode(){
 	$defaultText = htmlentities($_GET['keywords']);
-	
 	echo '
-		<header id ="main_header">
+		<header id="main_header">
 			<div id="rightAlign">
-			
 	';
-	
-	//links will be here. Check if user is logged in or logged out. Depending on that link will be displayed.
-	topRightLinks(); 
+	topRightLinks();
 	echo "
 			</div>
-			<a href =\"index.php\"><img src = \"images/mainLogo.png\"></a>
+			<a href=\"index.php\"><img src=\"images/mainLogo.png\"></a>
 		</header>
 		
-		<div id = \"top_search\">
+		<div id=\"top_search\">
 			<form name=\"input\" action=\"search.php\" method=\"get\">
-				<input type=\"text\" id=\"keywords\" name=\"keywords\" size=\"100\" class=\"searchBox\"
-				value=\"$defaultText\"> &nbsp;
-				
+				<input type=\"text\" id=\"keywords\" name=\"keywords\" size=\"100\" class=\"searchBox\" value=\"$defaultText\"> &nbsp;
 				<select id=\"category\" name=\"category\" class=\"searchBox\">
-	"; //category id css is missing
-	
-	//include categories here
+	";
 	createCategoryList();
 	echo '
 				</select>
@@ -36,48 +28,32 @@ function headerAndSeachCode(){
 
 //Top Right Links
 function topRightLinks(){
-	//If user is logged out
-	if(!isset($_SESSION['user_id'])){
-		echo '<a href="register.php"> Register </a> | <a href="login.php"> Log In </a>' ;
-	}
-	else{//if user is logged in
-		$user = $_SESSION['user_id'];
-		$result = mysql_query("SELECT * FROM messages WHERE receiver = $user AND status = 'unread'" or die(mysql_error) );
-		
-		$num = mysql_num_rows($result );
-		if($num == 0){
-			echo '<a href = "messages_inbox.php">Messages</a> |';
+	if( !isset($_SESSION['user_id']) ){
+		echo '<a href="register.php">Register</a> | <a href="login.php">Log In</a>';
+	}else{
+		$x = $_SESSION['user_id'];
+		$result = mysql_query("SELECT * FROM messages WHERE receiver=$x AND status='unread' ") or die(mysql_error());
+		$num = mysql_num_rows($result);
+		if($num==0){
+			echo '<a href="messages_inbox.php">Messages</a> |';
+		}else{
+			echo "<span class=\"usernames\"><a href=\"messages_inbox.php\">Messages($num)</a></span> |";
 		}
-		else{
-			echo "<span class =\"usernames\"><a href = \"messages_inbox.php\">Messages($num)</a></span> |";
-		}
-		
-		echo '<a href="additem.php">Add Item</a> | <a href="account.php">My Account</a> | <a href="logout.php">Log out</a>';
+		echo '<a href="additem.php">Add Item</a> | <a href="account.php">My Account</a> | <a href="logout.php">Log Out</a>';
 	}
 }
 
-//Create Category <option>'s for search bar
-
+//Creates Category <option>'s for search bar
 function createCategoryList(){
-	
-	if ( ctype_digit($_GET['category'])) {
-		$x = $_GET['category'];
-	}
-	else{
-		$x = 999;
-	}
-	
+	if( ctype_digit($_GET['category']) ){ $x = $_GET['category']; }else{ $x = 999; }
 	echo "<option>All Categories</option>";
-	
-	$i= 0;
-	
+	$i=0;
 	while(1){
-		if(numberToCategory($i) == "Category Does Not Exist"){
+		if(numberToCategory($i)=="Category Does Not Exist"){
 			break;
-		}
-		else{
-			echo " <option value = \"$i\" ";
-			if($i==$x){echo 'SELECTED';}
+		}else{
+			echo " <option value=\"$i\" ";
+			if($i==$x){echo ' SELECTED ';}
 			echo " > ";
 			echo numberToCategory($i);
 			echo "</option>";
@@ -85,53 +61,104 @@ function createCategoryList(){
 		$i++;
 	}
 }
+
 //Category Number to String
 function numberToCategory($n){
-	switch ($n){
-		case 0:
-			$cat = "Autobiography";
-			break;
-		case 1:
-			$cat = "Biography";
-			break;
-		case 2: 
-			$cat = "Fantasy";
-			break;
-		case 3: 
-			$cat = "Historical Fiction";
-			break;
-		case 4:
-			$cat = "Informational";
-			break;
-		case 5:
-			$cat = "Mystery & Adventure";
-			break;
-		case 6: 
-			$cat = "Poetry";
-			break;
-		case 7: 
-			$cat = "Realistic Fiction";
-			break;
-		case 8: 
-			$cat = "Science Fiction";
-			break;
-		default:
-			$cat = "Category Does Not Exist";
+	switch($n){
+    case 0:
+        $cat = "Antiques";
+        break;
+    case 1:
+        $cat = "Art";
+        break;
+    case 2:
+        $cat = "Automotive";
+        break;
+	case 3:
+        $cat = "Baby";
+        break;
+	case 4:
+        $cat = "Books";
+        break;
+	case 5:
+        $cat = "Business & Industrial";
+        break;
+	case 6:
+        $cat = "Cameras & Photo";
+        break;
+	case 7:
+        $cat = "Clothing & Accessories";
+        break;
+	case 8:
+        $cat = "Collectibles";
+        break;
+	case 9:
+        $cat = "Computers";
+        break;
+	case 10:
+        $cat = "Crafts";
+        break;
+	case 11:
+        $cat = "DVD's & Movies";
+        break;
+    case 12:
+        $cat = "Electronics";
+        break;
+	case 13:
+        $cat = "Health & Beauty";
+        break;
+	case 14:
+        $cat = "Home & Garden";
+        break;
+	case 15:
+        $cat = "Jewelry & Watches";
+        break;
+	case 16:
+        $cat = "Music";
+        break;
+	case 17:
+        $cat = "Pet Supplies";
+        break;
+	case 18:
+        $cat = "Services";
+        break;
+	case 19:
+        $cat = "Sports & Outdoors";
+        break;
+	case 20:
+        $cat = "Sports Memorabilia & Cards";
+        break;
+	case 21:
+        $cat = "Tools & Home Improvement";
+        break;
+    case 22:
+        $cat = "Toys & Hobbies";
+        break;
+	case 23:
+        $cat = "Video Games";
+        break;
+	case 24:
+        $cat = "Other";
+        break;
+	default:
+        $cat = "Category Does Not Exist";
 	}
+	
+	return $cat;
 }
 
-//Code for footer
+//code for footer
 function footerCode(){
 	echo '
 		<footer id="main_footer">
 			<table>
 				<tr>
+					<td><a href="https://www.facebook.com/rsm12">Syed\'s Facebook</a></td>
+					<td><a href="https://twitter.com/6ixflix">Syed\'s Twitter</a></td>
 					<td><a href="https://www.youtube.com/channel/UCK7MGRRcyoK_aUKOLknZNCw">YouTube</a></td>
-					<td><a href="https://www.facebook.com/rsm12">Ifah Facebook</a></td>
-					<td><a href="https://www.twitter.com/6ixFlix">Twitter+</a></td>
 				</tr>
 			</table>
-		</footer>
+		<footer>
 	';
 }
 ?>
